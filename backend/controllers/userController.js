cfd4557cdf65f1a6bff875b324f913554381da70
getahun <147873183+getahunabr@users.noreply.github.com>
2025-11-10 11:36:21 -0800

    commiting

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,5 @@
 import bcrypt from "bcryptjs";
-import User from "../models/User.js";
+import User from "../models/User";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 dotenv.config();
