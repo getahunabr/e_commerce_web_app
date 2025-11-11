0249c50f6b3a1662a8d3b5273ee591cdd61514cd
getahun <147873183+getahunabr@users.noreply.github.com>
2025-11-11 09:46:32 -0800

    commiting

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -41,7 +41,7 @@ export const createUser = async (req, res) => {
     });
   } catch (error) {
     console.error("Error creating user:", error);
-    res.status(500).json({ message: "Server error" });
+    res.status(500).json({ message: error.message });
   }
 };
 
