0bd5dddcf4fc5f8c47d13699b3a5361aed720e7f
getahun <147873183+getahunabr@users.noreply.github.com>
2025-11-11 04:30:31 -0800

    Rename user.js to UserModel.js temporarily

---
+++ backend/models/UserModel.js
@@ -0,0 +1,41 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../config/db.js";
+
+// Define the user model
+
+const User = sequelize.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    isAdmin: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+  },
+  {
+    timestamps: true,
+    tableName: "users",
+  }
+);
+
+export default User;
